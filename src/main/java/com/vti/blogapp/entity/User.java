810e8cedc45a9754a8502fdbe0a687489614499c
Nhân <nhan652011@gmail.com>
2023-12-16 20:58:26 +0700

    [Add] User entity

---
+++ src/main/java/com/vti/blogapp/entity/User.java
@@ -0,0 +1,40 @@
+package com.vti.blogapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "username", length = 20, unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "email", length = 50, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 72, nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
