6bc81089eb195144b3db35e49dd8fdf0b4778156
Nhân <nhan652011@gmail.com>
2023-12-19 19:58:50 +0700

    [Add] Login using database

--- src/main/java/com/vti/blogapp/repository/UserRepository.java
+++ src/main/java/com/vti/blogapp/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.vti.blogapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username); // dùng METHOD NAMES
 }
--- src/main/java/com/vti/blogapp/service/UserServiceImpl.java
+++ src/main/java/com/vti/blogapp/service/UserServiceImpl.java
@@ -5,12 +5,17 @@ import com.vti.blogapp.form.UserCreateForm;
 import com.vti.blogapp.mapper.UserMapper;
 import com.vti.blogapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService { // Riêng User phải cho impl thêm interface UserDetailsService này
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     @Override
@@ -20,5 +25,16 @@ public class UserServiceImpl implements UserService {
         user.setPassword(encodedPassword); // sau khi mã hoá thì gán vào user
         var savedUser = userRepository.save(user);
         return UserMapper.map(savedUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user == null) { // Nếu ko tìm thấy user thì throw ra lỗi này
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(), // nếu có thì return User truyền vào tài khoản, password
+                user.getPassword(),
+                AuthorityUtils.NO_AUTHORITIES); // Tất cả đều là user có quyền hạn giống nhau
     }
 }
